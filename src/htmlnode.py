5d0a3cb19056866a42179ba848b0f3e457687168
macrespo42 <macrespo@student.42.fr>
2024-09-21 22:19:31 +0200

    feat: ParentNode

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -17,7 +17,7 @@ class HTMLNode:
     def to_html(self):
         raise NotImplementedError()
 
-    def props_to_html(self):
+    def props_to_html(self) -> str:
         html = []
         for key, value in self.props.items():
             html.append(f'{key}="{value}"')
@@ -36,7 +36,7 @@ class LeafNode(HTMLNode):
     ) -> None:
         super().__init__(tag, value, None, props)
 
-    def to_html(self):
+    def to_html(self) -> str:
         if self.value is None:
             raise ValueError("Invalid HTML: no value")
         elif self.tag is None:
@@ -44,3 +44,25 @@ class LeafNode(HTMLNode):
         elif self.props is None or len(self.props) == 0:
             return f"<{self.tag}>{self.value}</{self.tag}>"
         return f"<{self.tag} {self.props_to_html()}>{self.value}</{self.tag}>"
+
+
+class ParentNode(HTMLNode):
+    def __init__(
+        self,
+        tag: str,
+        children: list[Any],
+        props: dict = None,
+    ) -> None:
+        super().__init__(tag, None, children, props)
+
+    def to_html(self) -> str:
+        if self.tag is None:
+            raise ValueError("Invalid HTML: no tag")
+        if self.children is None or len(self.children) == 0:
+            raise ValueError("Invalid HTML: no children")
+        childs = [child.to_html() for child in self.children]
+        childs = "".join(childs)
+        return f"<{self.tag}>{childs}</{self.tag}>"
+
+    def __repr__(self) -> str:
+        return f"ParentNode({self.tag}, {self.children}, {self.props})"
--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,6 +1,6 @@
 import unittest
 
-from htmlnode import HTMLNode, LeafNode
+from htmlnode import HTMLNode, LeafNode, ParentNode
 
 
 class TestHTMLNode(unittest.TestCase):
@@ -39,3 +39,55 @@ class TestLeafNode(unittest.TestCase):
     def test_html_without_tags(self):
         leaf = LeafNode(None, "This is a paragraph of text.")
         self.assertEqual(leaf.to_html(), "This is a paragraph of text.")
+
+
+class TestParentNode(unittest.TestCase):
+    def test_to_html(self):
+        node = ParentNode(
+            "p",
+            [
+                LeafNode("b", "Bold text"),
+                LeafNode(None, "Normal text"),
+                LeafNode("i", "italic text"),
+                LeafNode(None, "Normal text"),
+            ],
+        )
+        self.assertEqual(
+            node.to_html(),
+            "<p><b>Bold text</b>Normal text<i>italic text</i>Normal text</p>",
+        )
+
+    def test_to_html_no_tag(self):
+        node = ParentNode(
+            None,
+            [
+                LeafNode("b", "Bold text"),
+                LeafNode(None, "Normal text"),
+                LeafNode("i", "italic text"),
+                LeafNode(None, "Normal text"),
+            ],
+        )
+        self.assertRaises(
+            ValueError,
+            node.to_html,
+        )
+
+    def test_to_html_no_children(self):
+        node = ParentNode(
+            "p",
+            [],
+        )
+        self.assertRaises(
+            ValueError,
+            node.to_html,
+        )
+
+    def test_to_html_none_childs(self):
+        node = ParentNode(
+            "p",
+            None,
+        )
+        self.assertRaises(
+            ValueError,
+            node.to_html,
+        )

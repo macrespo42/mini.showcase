aa7a0b06c5fe7fc8efd171a6c2ed3a650e4f48cd
macrespo42 <macrespo@student.42.fr>
2024-09-26 14:06:29 +0200

    feat: detect block type algorithm

--- src/block_markdown.py
+++ src/block_markdown.py
@@ -2,3 +2,72 @@ def markdown_to_block(text: str) -> list[str]:
     blocks = text.split("\n\n")
     blocks = [block.strip() for block in blocks if block != ""]
     return blocks
+
+
+def is_header(block: str) -> bool:
+    if len(block) == 0:
+        return False
+    heading_level = 0
+    if block[0] == "#":
+        while heading_level < len(block) and block[heading_level] == "#":
+            heading_level += 1
+        if block[heading_level] != " " or heading_level > 6:
+            return False
+        else:
+            return True
+    else:
+        return False
+
+
+def is_code(block: str) -> bool:
+    if len(block) < 6:
+        return False
+    if block[0:3] == "```" and block[-3:] == "```":
+        lines = block.split("\n")
+        for line in lines[1:-1]:
+            if line[0] != ">":
+                return False
+        return True
+
+    return False
+
+
+def is_unordered_list(block: str) -> bool:
+    if len(block) == 0:
+        return False
+    lines = block.split("\n")
+    for line in lines:
+        if len(line) < 2:
+            return False
+        if not (line[0:2] == "* " or line[0:2] == "- "):
+            return False
+    return True
+
+
+def is_ordered_list(block: str) -> bool:
+    if len(block) == 0:
+        return False
+    lines = block.split("\n")
+    current_list_index = 0
+    for line in lines:
+        if len(line) < 2:
+            return False
+        if not (line[0].isnumeric() and line[1] == "."):
+            return False
+        if not (current_list_index + 1 == int(line[0])):
+            return False
+        current_list_index += 1
+    return True
+
+
+def block_to_block_type(block: str) -> str:
+    if is_header(block):
+        return "header"
+    elif is_code(block):
+        return "code"
+    elif is_unordered_list(block):
+        return "unordered_list"
+    elif is_ordered_list(block):
+        return "ordered_list"
+    else:
+        return "p"
--- src/test_block_markdown.py
+++ src/test_block_markdown.py
@@ -1,6 +1,6 @@
 import unittest
 
-from block_markdown import markdown_to_block
+from block_markdown import block_to_block_type, markdown_to_block
 
 
 class TestMarkdownToBlock(unittest.TestCase):
@@ -126,3 +126,54 @@ class TestMarkdownToBlock(unittest.TestCase):
         * This is another list item""",
             ],
         )
+
+
+class TestBlockToBlockType(unittest.TestCase):
+    def test_header(self):
+        block = "### hello world!"
+        self.assertEqual(block_to_block_type(block), "header")
+
+    def test_invalid_header(self):
+        block = "###helloworld"
+        self.assertEqual(block_to_block_type(block), "p")
+
+        block = "####### helloworld"
+        self.assertEqual(block_to_block_type(block), "p")
+
+    def test_code(self):
+        block = "```> code1\n> code2\n> code3```"
+        self.assertEqual(block_to_block_type(block), "code")
+
+    def test_invalid_code(self):
+        block = "```> code1\ncode2\n> code3```"
+        self.assertEqual(block_to_block_type(block), "p")
+        block = ""
+        self.assertEqual(block_to_block_type(block), "p")
+        block = "`````"
+        self.assertEqual(block_to_block_type(block), "p")
+
+    def test_unordered_list(self):
+        block = "* lol\n* tbh\n* tbh"
+        self.assertEqual(block_to_block_type(block), "unordered_list")
+        block = "- lol\n- tbh\n- tbh"
+        self.assertEqual(block_to_block_type(block), "unordered_list")
+        block = "- lol\n- tbh\n* tbh"
+        self.assertEqual(block_to_block_type(block), "unordered_list")
+
+    def test_invalid_unordered_list(self):
+        block = "*lol\n* tbh\n* tbh"
+        self.assertEqual(block_to_block_type(block), "p")
+        block = "-Alol\n- tbh\n- tbh"
+        self.assertEqual(block_to_block_type(block), "p")
+
+    def test_ordered_list(self):
+        block = "1. lol\n2. tbh\n3. tbh"
+        self.assertEqual(block_to_block_type(block), "ordered_list")
+
+    def test_invalid_ordered_list(self):
+        block = "1. lol\n4. tbh\n3. tbh"
+        self.assertEqual(block_to_block_type(block), "p")
+        block = "1lol\n4. tbh\n3. tbh"
+        self.assertEqual(block_to_block_type(block), "p")
+        block = "0. lol\n1. tbh\n3. tbh"
+        self.assertEqual(block_to_block_type(block), "p")
